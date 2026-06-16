dc1e21815a2d082f0c7d7c6987c60c3bf1e6064d
Charan89781 <charansai87654@gmail.com>
2026-06-16 11:21:59 +0530

    Cleanup root files after structured folder separation

--- lib/domain/repositories/space_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/space_entity.dart';
-import '../entities/booking_entity.dart';
-
-abstract class SpaceRepository {
-  Stream<List<SpaceEntity>> getSpaces();
-  Future<void> bookSpace(BookingEntity booking);
-  Future<void> listSpace(SpaceEntity space);
-  Stream<List<BookingEntity>> getUserBookings(String userId);
-}
--- lib/presentation/providers/storage_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../core/services/storage_service.dart';
-
-final storageProvider = Provider((ref) => StorageService());
