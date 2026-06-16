f0bda624d295fa2c382bcf2b7c85f7d867b58a2f
Charan89781 <charansai87654@gmail.com>
2026-06-16 13:25:06 +0530

    Fix CI: remove --web-renderer flag and hardcoded Windows D: drive path in gradle

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -5,17 +5,6 @@ allprojects {
     }
 }
 
-// Keep ALL build output on D: drive
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("D:/AndroidFiles/Projects/LocalSync3/build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
 subprojects {
     project.evaluationDependsOn(":app")
 }
